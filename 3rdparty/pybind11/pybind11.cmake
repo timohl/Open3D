11de72a2544e502c9527c8d32ebb232c606fdf48
Tim Ohliger <ohliger.tim@gmail.com>
2025-01-11 03:05:25 +0100

    Using forked pybind11 with numpy typing fixes (temporary for testing until merged upstream)

--- 3rdparty/pybind11/pybind11.cmake
+++ 3rdparty/pybind11/pybind11.cmake
@@ -3,8 +3,8 @@ include(FetchContent)
 FetchContent_Declare(
     ext_pybind11
     PREFIX pybind11
-    URL https://github.com/pybind/pybind11/archive/refs/tags/v2.13.1.tar.gz
-    URL_HASH SHA256=51631e88960a8856f9c497027f55c9f2f9115cafb08c0005439838a05ba17bfc
+    GIT_REPOSITORY https://github.com/timohl/pybind11.git
+    GIT_TAG origin/numpy-typing
     DOWNLOAD_DIR "${OPEN3D_THIRD_PARTY_DOWNLOAD_DIR}/pybind11"
 )
 
